f5f27bf9f7ab4013eb8b3e3a57bc53432304edc4
ningen <msg1801022@gmail.com>
2023-01-07 00:28:59 +0900

    [add] packages

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,10 +1,13 @@
 import Header from "../components/Header.tsx";
+import { ChakraProvider } from "chakra-ui";
 
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      {children}
+      <ChakraProvider>
+        {children}
+      </ChakraProvider>
     </>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,7 @@
 import { Head, Link } from "aleph/react";
+import { Center, Box, Input, Button } from "chakra-ui";
+
+import { useState } from "react"
 
 const externalLinks = [
   ["Get Started", "https://alephjs.org/docs/get-started"],
@@ -7,6 +10,30 @@ const externalLinks = [
 ];
 
 export default function Index() {
+  const [email, setEmail] = useState<string>("");
+
+  const onChangeEmail: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const onSubmit: React.FormEventHandler<HTMLButtonElement> = (_) => {
+    console.log(email)
+    setEmail("")
+  }
+
+  return (
+    <Center>
+      <Box>
+          <Input variant="outline" value={email} onChange={onChangeEmail} />
+          <Button colorScheme="teal" variant="solid" onClick={onSubmit} >
+            送信 
+          </Button>
+      </Box>
+    </Center>
+  ) 
+}
+
+function _Index() {
   return (
     <div className="screen index">
       <Head>
